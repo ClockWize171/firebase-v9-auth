71b4870718c8b30ce4ff22725ec24532ffbf24b7
ClockWize171 <jupiterminhtin998@gmail.com>
2022-05-10 23:30:29 +0630

    Login page built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import {
+  Container,
+  Text,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input
+} from '@chakra-ui/react';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const emailRef = useRef();
+  const passwdRef = useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW='container.md'>
+      <Box className='vertical-center'>
+        <FormControl>
+          <Text fontSize={['2xl','3xl']} fontWeight='bold'>
+            Firebase v9 Basic Authentication
+          </Text>
+          <FormLabel mt={2} htmlFor='email'>Email address</FormLabel>
+          <Input id='email' ref={emailRef} isRequired type='email' />
+          <FormLabel mt={2} htmlFor='password'>Password</FormLabel>
+          <Input id='password' ref={passwdRef} isRequired type='password' />
+          <Button
+            onClick={()=> console.log(emailRef, passwdRef)}
+            mt={4}
+            colorScheme='teal'
+            type='submit'>
+            Login
+          </Button>
+          <Button
+            mt={4}
+            ml={3}
+            colorScheme='blue'
+            type='submit'>
+            Register
+          </Button>
+        </FormControl>
+      </Box>
+    </Container>
   );
 }
 
