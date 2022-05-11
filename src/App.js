e327d0592090a6d0dc90f06058291ad96253001e
ClockWize171 <jupiterminhtin998@gmail.com>
2022-05-12 00:07:13 +0630

    added register page and start firebase email signup

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
+import LogIn from './pages/LogIn/LogIn';
+import Register from './pages/Register/Register';
 import {
-  Container,
-  Text,
-  Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input
-} from '@chakra-ui/react';
-import { useRef } from 'react';
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 
 function App() {
 
-  const emailRef = useRef();
-  const passwdRef = useRef();
-
   return (
-    <Container maxW='container.md'>
-      <Box className='vertical-center'>
-        <FormControl>
-          <Text fontSize={['2xl','3xl']} fontWeight='bold'>
-            Firebase v9 Basic Authentication
-          </Text>
-          <FormLabel mt={2} htmlFor='email'>Email address</FormLabel>
-          <Input id='email' ref={emailRef} isRequired type='email' />
-          <FormLabel mt={2} htmlFor='password'>Password</FormLabel>
-          <Input id='password' ref={passwdRef} isRequired type='password' />
-          <Button
-            onClick={()=> console.log(emailRef, passwdRef)}
-            mt={4}
-            colorScheme='teal'
-            type='submit'>
-            Login
-          </Button>
-          <Button
-            mt={4}
-            ml={3}
-            colorScheme='blue'
-            type='submit'>
-            Register
-          </Button>
-        </FormControl>
-      </Box>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<LogIn />}></Route>
+        <Route  path='/register' element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
